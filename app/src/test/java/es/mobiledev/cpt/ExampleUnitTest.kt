4ee30110c9f7697b925b00946f9db47ce0c55f6e
ManuelJCGomez <manueljcgomez@gmail.com>
2025-06-30 18:46:11 +0200

    Implemented ktLint

--- app/src/test/java/es/mobiledev/cpt/ExampleUnitTest.kt
+++ app/src/test/java/es/mobiledev/cpt/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package es.mobiledev.cpt
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
