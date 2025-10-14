31c7f42a8e5560eeaf1e97b2e80ba1d32408b936
ManuelJCGomez <manueljcgomez@gmail.com>
2025-10-14 19:33:15 +0200

    Add KDoc to BaseViewModel and UiState

--- commonAndroid/src/main/java/es/mobiledev/commonandroid/ui/base/UiState.kt
+++ commonAndroid/src/main/java/es/mobiledev/commonandroid/ui/base/UiState.kt
@@ -1,5 +1,12 @@
 package es.mobiledev.commonandroid.ui.base
 
+/**
+ * Represents the state of the UI.
+ *
+ * @param T The type of the data held by the UI state.
+ * @property data The data to be displayed in the UI.
+ * @property isLoading A boolean that indicates if a data load is in progress.
+ */
 data class UiState<T>(
     val data: T,
     val isLoading: Boolean = false,
